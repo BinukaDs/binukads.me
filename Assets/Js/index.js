3e96ed7ed1c0ed0216f223f3ad70a60c9a5ad712
Binuka Dasunpriya <binukadasunpriya@gmail.com>
2022-04-13 16:38:24 +0530

    updated bugs

--- Assets/Js/index.js
+++ Assets/Js/index.js
@@ -3,12 +3,12 @@ function select() {
 
     if (x.matches) {
         document.getElementById('whatsapp_desktop').href = 'https://api.whatsapp.com/send?phone=94755408993&text=Hi!%20This%20message%20from%20BinukaDs.me%20website%20:D'
-        document.getElementById('whatsapp_desktop2').href = 'https://api.whatsapp.com/send?phone=94755408993&text=Hi!%20This%20message%20from%20BinukaDs.me%20website%20:D'
+        document.getElementById('whatsapp_desktop_2').href = 'https://api.whatsapp.com/send?phone=94755408993&text=Hi!%20This%20message%20from%20BinukaDs.me%20website%20:D'
     }
 
     else{
         document.getElementById('whatsapp_desktop').href = 'https://web.whatsapp.com/send?phone=94755408993&text=Hi!%20This%20message%20from%20BinukaDs.me%20website%20:D' 
-        document.getElementById('whatsapp_desktop2').href = 'https://web.whatsapp.com/send?phone=94755408993&text=Hi!%20This%20message%20from%20BinukaDs.me%20website%20:D' 
+        document.getElementById('whatsapp_desktop_2').href = 'https://web.whatsapp.com/send?phone=94755408993&text=Hi!%20This%20message%20from%20BinukaDs.me%20website%20:D' 
     }
 }
 
