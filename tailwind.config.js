257daf7e73d4e8dd15e7b03c6099dd90fa7aa00c
Binuka Dasunpriya <binukadasunpriya@gmail.com>
2022-06-22 16:03:48 +0530

    updated whole website

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      zIndex: {
+        '9': '9',
+      },
+
+      colors: {
+        bg: '#131515',
+        highlight: '#C1C6BE',
+        hover: '#2B363B',
+        bgColor: '#1B2123'
+      },
+
+      grayscale: {
+        70: '70%'
+      }
+
+    },
+  },
+  plugins: [],
+}
