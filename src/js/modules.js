79fdb7bc0ddfc1c28eef59db433c82069b0c0fcf
Binuka Dasunpriya <binukadasunpriya@gmail.com>
2023-01-15 14:10:41 +0530

    temporary updated (2)

--- src/js/modules.js
+++ src/js/modules.js
@@ -3,9 +3,9 @@ class MyFooter extends HTMLElement {
       this.innerHTML = `
           <footer class="mt-5 mb-5">
         
-          <h4 class="text-center text-white">"Simplycity is the best!"</h4>
+          <h4 class="text-center text-white">"Make it Simple!"</h4>
           <h4 class="text-center text-white">Coded with ❤️ By BinukaDs.</h4>
-          <h4 class="text-center md:mb-5 text-white">&copy; BinukaDs  2022 | All Rights Reserved.</h4>
+          <h4 class="text-center md:mb-5 text-white">&copy; BinukaDs  2023 | All Rights Reserved.</h4>
           
           </footer>
     `
